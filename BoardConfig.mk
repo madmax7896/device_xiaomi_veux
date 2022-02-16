f1781bce8466ff05bd29828e6f8d3fa699b7366f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-02-16 16:07:54 +0530

    veux: Adapt DT2W extension to AOSPA power HAL
    https://github.com/pa-gr/android_vendor_aospa/commit/027c4d9fea9bce9df0b8de6314e03934d97f8652

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_ODM := odm
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_POWER_FEATURE_EXT_LIB := //$(DEVICE_PATH):libpowerfeature_ext_veux
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
