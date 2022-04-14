6e9f7f4ffbcc2e416ae5be48387bd3b8ff8ba04b
madmax7896 <madmax78968@gmail.com>
2022-04-14 13:11:36 +0000

    veux: Build fastbootd

--- device.mk
+++ device.mk
@@ -73,6 +73,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.3.vendor
 
+# Fastboot
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Filesystems table
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default \
