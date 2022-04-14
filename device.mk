c66cb3745175cfaee28328687600b6f09020f088
madmax7896 <madmax78968@gmail.com>
2022-04-14 13:35:39 +0000

    veux: Build gatekeeper and keymaster HIDL

--- device.mk
+++ device.mk
@@ -136,6 +136,11 @@ PRODUCT_VENDOR_KERNEL_HEADERS += $(LOCAL_PATH)-kernel/kernel-headers
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@4.1.vendor
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
