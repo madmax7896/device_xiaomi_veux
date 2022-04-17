fad382c89b30492f7cd97d1f024b44cbdeebc15b
madmax7896 <madmax78968@gmail.com>
2022-04-17 08:25:16 +0000

    veux: Import wifi configs from stock

--- device.mk
+++ device.mk
@@ -233,3 +233,8 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf
