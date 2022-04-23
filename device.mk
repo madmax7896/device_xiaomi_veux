16e74386cfe68e9a176784e89e49676a9862c4e2
Daniel Rosenberg <drosen@google.com>
2022-04-23 14:52:33 +0000

    veux: Enable Project ID Quota support on userdata
    * These features enable wide inodes on userdata, which
      is required for project ID quotas. Wide inodes can
      only be set at MKFS time.
    
    * This also enables casefolding, which is required for the
      removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67

--- device.mk
+++ device.mk
@@ -189,6 +189,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Platform
 TARGET_BOARD_PLATFORM := holi
 
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # QC common
 TARGET_COMMON_QTI_COMPONENTS := \
     audio \
